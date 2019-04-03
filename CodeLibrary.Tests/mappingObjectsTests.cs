71acdb0128bf74908eb53371ae84c3a83fe9932a
deliorolo <delio.rolo@gmail.com>
2019-04-03 19:47:26 +0200

    Unit testing - mapping objects methods

--- CodeLibrary.Tests/mappingObjectsTests.cs
+++ CodeLibrary.Tests/mappingObjectsTests.cs
@@ -2,6 +2,8 @@
 using CodeLibrary.ModelsMVC;
 using Xunit;
 using FluentAssertions;
+using Moq;
+using CodeLibrary.DataAccess;
 
 namespace CodeLibrary.Tests
 {
@@ -19,9 +21,35 @@ namespace CodeLibrary.Tests
 
             ISoldProductAccomplishedModel soldProductAccomplished = MappingObjects.SoldProductToSoldProductAccomplished(soldProduct);
 
-            Assert.Equal(soldProductAccomplished.Name, soldProduct.Name);
-            Assert.Equal(soldProductAccomplished.Price, soldProduct.Price);
+            Assert.Equal(soldProduct.Name, soldProductAccomplished.Name);
+            Assert.Equal(soldProduct.Price, soldProductAccomplished.Price);
             soldProductAccomplished.Category.Should().BeEquivalentTo(soldProduct.Category);
+        }
+
+        [Fact]
+        public void ProductToSoldProduct_ShouldBeMapped()
+        {
+            IProductModel product = Factory.InstanceProductModel();           
+
+            product.Name = "some product";
+            product.Price = 888.45M;
+            product.CategoryID = 654;
+            product.Category.ID = 654;
+            product.Category.Name = "some category";
+
+            int idTable = 36;
+
+            Mock<IDataAccessSubCategory<ITableModel>> data = new Mock<IDataAccessSubCategory<ITableModel>>();
+            data.Setup(x => x.FindById(idTable)).
+                Returns(new TableModel() { ID = 36, AreaID = 7, NumberOfTable = 9765, Occupied = true, Area = new AreaModel { ID = 7, Name = "some area" } });
+
+            ISoldProductModel soldProduct = MappingObjects.ProductToSoldProduct(product, idTable, data.Object);
+
+            Assert.Equal(product.Name, soldProduct.Name);
+            Assert.Equal(product.Price, soldProduct.Price);
+            Assert.Equal("", soldProduct.Detail);
+            soldProduct.Category.Should().BeEquivalentTo(product.Category);
+            soldProduct.Table.Should().BeEquivalentTo(data.Object.FindById(idTable));
         }
     }
 }
