66c7533bc602342f45fccc37cdbbe465e2a2b160
deliorolo <delio.rolo@gmail.com>
2019-04-03 15:27:50 +0200

    Adding first unit tests

---
+++ CodeLibrary.Tests/mappingObjectsTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeLibrary.AccessoryCode;
+using CodeLibrary.ModelsMVC;
+using Xunit;
+using FluentAssertions;
+
+namespace CodeLibrary.Tests
+{
+    public class MappingObjectsTests
+    {
+        [Fact]
+        public void SoldProductToSoldProductAccomplished_ShouldBeMapped()
+        {
+            ISoldProductModel soldProduct = Factory.InstanceSoldProductModel();
+            soldProduct.Name = "some product";
+            soldProduct.Price = 999.93M;
+            soldProduct.Category.Name = "some category";
+            soldProduct.Category.ID = 897;
+            soldProduct.CategoryID = 897;
+
+            ISoldProductAccomplishedModel soldProductAccomplished = MappingObjects.SoldProductToSoldProductAccomplished(soldProduct);
+
+            Assert.Equal(soldProductAccomplished.Name, soldProduct.Name);
+            Assert.Equal(soldProductAccomplished.Price, soldProduct.Price);
+            soldProductAccomplished.Category.Should().BeEquivalentTo(soldProduct.Category);
+        }
+    }
+}
